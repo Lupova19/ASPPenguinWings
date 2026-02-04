9df66f075636e1f0e7189cc1436556b651832c6c
Lupova19 <nadiqlupova@gmail.com>
2026-02-04 12:30:11 +0200

    Updated Customer and DataBase name

--- ASPPenguinWings/Data/Migrations/20260129100854_firstmigration.Designer.cs
+++ ASPPenguinWings/Data/Migrations/20260129100854_firstmigration.Designer.cs
@@ -4,6 +4,7 @@ using ASPPenguinWings.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASPPenguinWings.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260129100854_firstmigration")]
+    partial class firstmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -58,6 +61,7 @@ namespace ASPPenguinWings.Data.Migrations
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Email")
+                        .IsRequired()
                         .HasMaxLength(256)
                         .HasColumnType("nvarchar(256)");
 
@@ -83,6 +87,10 @@ namespace ASPPenguinWings.Data.Migrations
                         .HasColumnType("nvarchar(256)");
 
                     b.Property<string>("PasswordHash")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Phone")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("PhoneNumber")
--- ASPPenguinWings/Models/Customer.cs
+++ ASPPenguinWings/Models/Customer.cs
@@ -6,8 +6,6 @@ namespace ASPPenguinWings.Models
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
         public DateTime DateOn { get; set; }
         public ICollection<Order> Orders { get; set; }
     }
